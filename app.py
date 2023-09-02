4ea91f405039fd49f4d494a36dd86339c9c7bc04
Heidi Fryzell <hfryzell@gmail.com>
2023-09-02 10:52:35 -0400

    intital setup of static and template files

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+@app.route('/<name>')
+
+def hello_world(name="Cutie"):
+    return render_template("index.html", name=name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+
+def add(num1, num2):
+    return render_template("add.html", num1=num1, num2=num2)

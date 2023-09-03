f69a9e2dfce827d9d2ff07a44bfee612386c149d
Heidi Fryzell <hfryzell@gmail.com>
2023-09-02 21:16:45 -0400

    add dish and guest list forms built and failure page

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, request
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__) #turn this file into a flask application
+
+db = SQL("sqlite:///brunches.db")
+
+# ATTENDEES = {}
+
+DISHTYPES = [
+    "Carbohydrate",
+    "Protein",
+    "Fruit",
+    "Beverage"
+]
 
 @app.route('/')
 # @app.route('/<name>')
@@ -15,3 +27,23 @@ def createEvent():
 @app.route('/add-guests', methods=["POST", "GET"])
 def addGuests():
     return render_template("add-guests.html")
+
+@app.route('/add-dish', methods=["POST", "GET"])
+def addDish():
+    if request.method == "POST":
+        # Validate submission
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        dish_type = request.form.get("dish-type")
+        dish = request.form.get("dish")
+        if not first_name or dish_type not in DISHTYPES:
+            return render_template("failure.html")
+
+        db.execute("INSERT INTO attendees (first_name, last_name, dish_type, dish) VALUES(?, ?, ?, ?)", first_name, last_name, dish_type, dish)
+
+        return redirect("/guest-list")
+    return render_template("add-dish.html", dishtypes=DISHTYPES)
+
+@app.route('/guest-list')
+def guestList():
+    return render_template("guest-list.html")

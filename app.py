b4d5a685b177f184d6c98caa1a54fafb6d06eb66
Heidi Fryzell <hfryzell@gmail.com>
2023-09-03 08:15:31 -0400

    create brunch list working

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ app = Flask(__name__) #turn this file into a flask application
 
 db = SQL("sqlite:///brunches.db")
 
-# ATTENDEES = {}
-
 DISHTYPES = [
     "Carbohydrate",
     "Protein",
@@ -17,16 +15,29 @@ DISHTYPES = [
 @app.route('/')
 @app.route('/<name>')
 
-def hello_world(name="Cutie"):
+def helloWorld(name="Cutie"):
     return render_template("index.html", name=name)
 
 @app.route('/create-event', methods=["POST", "GET"])
 def createEvent():
+    if request.method == "POST":
+        # Validate submission
+        name = request.form.get("event-name")
+        date = request.form.get("event-date")
+        time = request.form.get("event-time")
+        location = request.form.get("event-location")
+        if not name:
+            return render_template("failure.html")
+
+        db.execute("INSERT INTO brunches (name, date, time, location) VALUES(?, ?, ?, ?)", name, date, time, location)
+
+        return redirect("brunch-list")
     return render_template("create-event.html")
 
-@app.route('/add-guests', methods=["POST", "GET"])
-def addGuests():
-    return render_template("add-guests.html")
+@app.route('/brunch-list')
+def brunchList():
+    brunches = db.execute("SELECT * FROM brunches")
+    return render_template("brunch-list.html", brunches=brunches)
 
 @app.route('/add-dish', methods=["POST", "GET"])
 def addDish():
